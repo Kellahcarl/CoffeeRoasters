004ab5e485c7f9167e50cd26703c318c6124307d
baraka caleb <65470823+pimby@users.noreply.github.com>
2023-11-17 11:09:46 +0300

    finished html content next css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CreatePlanComponent } from './create-plan/create-plan.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'', component : HomeComponent
+}, {
+  path: 'aboutus', component : AboutUsComponent
+  },
+{path:'createplan' , component : CreatePlanComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CreatePlanComponent } from './create-plan/create-plan.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    CreatePlanComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
